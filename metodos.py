874eaf199bb7f26a37bb6c16d68243a99bd20309
pedro <pedrohensimao2@gmail.com>
2025-09-30 15:40:29 +0100

    Adding app, reqs and notes

---
+++ metodos.py
@@ -0,0 +1,9 @@
+#GET Requisita algo para o servidor, retorna apenas dados
+
+#POST Inserir informacoes no servidor
+
+#PUT Atualizar, substituir informacoes do cliente no servidor
+
+#PATCH Atualizacoes parciais, assim como o PUT substitui informacoes do cliente no servidor, mas somente oque for solicitado
+
+#DELETE Remover recursos do sistema
---
+++ responses.py
@@ -0,0 +1,11 @@
+#100-199 = Repostas informativas do servidor
+
+#200-299 = Respostas bem sucedidas
+
+#300-399 = Mensagens redirecionadas (tentar acessar um recurso que ja nao existe por exemplo)
+
+#400-499 = Respostas de erro do Cliente (cliente inseriu uma informacao com o formato errado por exemplo)
+
+#500-599 = Erros do servidor (cliente inseriu tudo corretamente entretanto houve um erro no servidor interno)
+
+#https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Reference/Status
